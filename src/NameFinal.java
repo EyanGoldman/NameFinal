b7fedcc5f06c8c515e88f53b6fc44cec23b031b7
Student <student@c02kw2anfh51.dalton.org>
2016-01-25 09:43:25 -0500

    damon's changes

--- src/NameFinal.java
+++ src/NameFinal.java
@@ -1,12 +1,12 @@
+import java.util.Random;
 import java.util.Scanner;
 public class NameFinal {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		char[] input = scan.nextLine().toCharArray();
 		hyphen(input);
-		System.out.println(hyphen(input));
+		System.out.println(random(input));
 		System.out.println("new changes");
-
 	}
 	//marco
 	static char[] reverse() 
@@ -31,8 +31,8 @@ public class NameFinal {
 		{
 			if(input[i]!='a' && input[i]!= 'A' && input[i]!= 'e' && input[i]!='E' && input[i]!='i' && input[i]!= 'I' 
 					&& input[i]!= 'o' && input[i]!= 'O' && input[i]!= 'u' && input[i]!= 'U')
-			/*
-			 
+				/*
+
 			 		input[i] == 'b' || input[i] == 'B' || input[i] == 'c' || input[i] == 'C' || input[i] == 'd' || 
 					input[i] == 'D' || input[i] == 'f' || input[i] == 'F' || input[i] == 'g' || input[i] == 'G' 
 					|| input[i] == 'h' || input[i] == 'H' || input[i] == 'j' || input[i] == 'J' || input[i] == 'k' 
@@ -42,7 +42,7 @@ public class NameFinal {
 					|| input[i] == 't' || input[i] == 'T' || input[i] == 'v' || input[i] == 'V' || input[i] == 'w' 
 					|| input[i] == 'W' || input[i] == 'x' || input[i] == 'X' || input[i] == 'y' || input[i] == 'Y' 
 					|| input[i] == 'z' || input[i] == 'Z'
-			 */
+				 */
 			{
 				consonents++;
 			}
@@ -101,17 +101,57 @@ public class NameFinal {
 	{
 		return null;
 	}
-	static char[] random()
+	static char[] random(char[] array)
 	{
-		return null;
+		Random gen = new Random();
+		char[] charray = new char[array.length];
+		int[] alreadypicked = new int[array.length];
+		for (int i = 0; i < alreadypicked.length; i++) {
+			alreadypicked[i] = -1;
+		}
+		boolean picked = true;
+		int choice = 0;
+		for (int i = 0; i < charray.length; i++) {//iterate across charray and set its values
+			picked = true;
+			while(picked){
+				picked = false;
+				choice = gen.nextInt(array.length);
+				for (int j = 0; j < alreadypicked.length; j++) {//iterate through all values that have been picked
+					if(choice == alreadypicked[j]){//check if this has been picked
+						picked = true;
+						System.out.println(charray);
+					}
+				}
+			}
+			alreadypicked[i] = choice;
+			charray[i] = array[choice];
+		}
+		return charray;
 	}
 	static boolean palindrome(char[] input)
 	{
 		return false;
 	}
-	static char[] sorted()
+	static char[] sorted(char[] array)
 	{
-		return null;
+		int[] intarray = new int[array.length];
+		int[] charray = new int[array.length];
+		boolean[] alreadyused = new boolean[array.length];
+		
+		int least = 10000;
+		for (int i = 0; i < array.length; i++) {
+			intarray[i] = (int) array[i];
+		}
+		for (int i = 0; i < intarray.length; i++) {
+			least = 10000;
+			for (int j = 0; j < intarray.length; j++) {
+				if(!alreadyused[j]){
+					
+				}
+			}
+			alreadyused[i] = true;
+		}
+		//return charray;
 	}
 	static char[] initials()
 	{
